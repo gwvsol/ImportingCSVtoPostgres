3d60f9371dc68ee19b513340c4aeb6d78266f7b4
gwvsol <gwvsol@gmail.com>
2021-04-28 14:35:29 +0300

    добавлен файл описания проекта

---
+++ import_coords.py
@@ -0,0 +1,12 @@
+from csv import DictReader
+
+file_csv = "coords.csv"
+
+# open file in read mode
+with open(file_csv, 'r') as read_obj:
+    # pass the file object to DictReader() to get the DictReader object
+    csv_dict_reader = DictReader(read_obj)
+    # iterate over each line as a ordered dictionary
+    for row in csv_dict_reader:
+        # row variable is a dictionary that represents a row in csv
+        print(row)
