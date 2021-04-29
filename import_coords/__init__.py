0fde024d6bd415df44a670fdac6697eea5aa2548
gwvsol <gwvsol@gmail.com>
2021-04-29 19:05:52 +0300

    добавлен файл для инициализации модуля

--- import_coords/__init__.py
+++ import_coords/__init__.py
@@ -1,1 +1,20 @@
-from .import_coords import *
+"""
+Модуль для импорта CSV файла в Postgres
+"""
+
+__author__ = 'Fedorov Mikhail'
+__maintainer__ = 'Fedorov Mikhail'
+
+__email__ = 'jwvsol@yandex.ru'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+    '__doc__',
+)
